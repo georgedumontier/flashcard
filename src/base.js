18221f4531106e9bd7e2e52495bd37be15166f9c
George DuMontier <gdumonti@meredith.com>
2019-05-06 16:45:57 -0500

    allows for deleting cards

--- src/base.js
+++ src/base.js
@@ -1,14 +1,7 @@
 import Rebase from "re-base";
 import firebase from "firebase";
 
-const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyBz4RoHTBDf5MsdaeWb4qQy-NvQp_gm1kQ",
-  authDomain: "flashcards-cb170.firebaseapp.com",
-  databaseURL: "https://flashcards-cb170.firebaseio.com",
-  projectId: "flashcards-cb170",
-  storageBucket: "flashcards-cb170.appspot.com",
-  messagingSenderId: "44448519659"
-});
+const firebaseApp = firebase.initializeApp({});
 
 const base = Rebase.createClass(firebaseApp.database());
 
